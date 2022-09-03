5889d5afc56fe49fc7e6c9085648671d3fb7987d
ScratchX98 <scratchx@gmx.com>
2022-09-03 14:56:47 +0200

    Use web hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from '@/components/Home.vue';
 import NewNote from '@/components/NewNote.vue';
 import Note from '@/components/Note.vue';
 
 const router = createRouter({
-  history: createWebHistory('/'),
-  mode: 'hash',
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
